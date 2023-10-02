b7483d6625a3a919cdce9ca323982f92cf95a1d2
Daniel Dublin <bots4fun1@gmail.com>
2023-10-02 15:33:55 +0300

    made the db credentials into a secure entries to .env

--- database/db.py
+++ database/db.py
@@ -1,14 +1,14 @@
 import aiomysql
-from config import DATABASE_URI
+import os
 
 # Initialize the database connection (async)
 async def init_database():
     pool = await aiomysql.create_pool(
-        host='localhost',
-        port=3306,
-        user='your_username',
-        password='your_password',
-        db='your_database',
+        host= os.getenv("DB_HOST"),
+        port= os.getenv("PORT"),
+        user= os.getenv("DB_USER"),
+        password= os.getenv("DB_PASSWORD"),
+        db= os.getenv("DB_NAME"),
         autocommit=True,
         charset='utf8mb4',
         cursorclass=aiomysql.DictCursor,
