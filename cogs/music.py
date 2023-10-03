2732456fc88c3154727d50bdaa1f38575424fe7c
Daniel Dublin <bots4fun1@gmail.com>
2023-10-03 23:33:08 +0300

    removed Categories from options.json made music.py work as intended someone please end me

--- cogs/music.py
+++ cogs/music.py
@@ -1,83 +1,189 @@
-import discord
+﻿import discord
 from discord.ext import commands, menus
 import json
 
-# Define the available categories and options
-categories = ["Info", "Chords"]
-degree = "\u00b0"
-
-options ={}
-# Load data from JSON file
-with open('cogs/options.json', 'r') as json_file:
-    options = json.load(json_file)
-
-
-
 class NotesMenu(menus.Menu):
-    def __init__(self, category=None, subcategory=None):
-        super().__init__()
-        self.category = category
-        self.subcategory = subcategory
+    def __init__(self, ctx, bot, json_data):
+        super().__init__(timeout=60.0, delete_message_after=True)
+        self.json_data = json_data
+        self.current_level = 0
+        self.selections = []
+        self.bot = bot
+        self.menu_message = None
+        self.author = ctx.author
+        self.page_index = 0
+        self.options_per_page = 7  # Number of options per page
 
-    async def send_initial_message(self, ctx, channel):
-        if self.category is None:
-            # Show category selection if no category selected
-            embed = discord.Embed(title="Select a category:", color=0x7e016f)
-            for i, category in enumerate(categories, start=1):
-                embed.add_field(name=f"{i}. {category}", value=f"Select {category}", inline=False)
-            embed.set_footer(text="React with the corresponding number to select a category.")
-            return await channel.send(embed=embed)
+        self.bot.add_listener(self.on_raw_reaction_add)
+
+    async def on_raw_reaction_add(self, payload):
+        if payload.user_id == self.bot.user.id:
+            return
+        if payload.user_id != self.author.id:
+            return
+
+        message_id = payload.message_id
+        channel_id = payload.channel_id
+        channel = self.bot.get_channel(channel_id)
+
+        if not channel:
+            return
+
+        try:
+            menu_message = await channel.fetch_message(message_id)
+        except discord.NotFound:
+            return
+
+        if menu_message.author != self.bot.user:
+            return
+
+        await menu_message.clear_reactions()
+
+        emoji = str(payload.emoji)
+        if emoji == "↩️" and self.current_level > 0:
+            self.current_level -= 1
+            self.selections.pop()
+        elif emoji == "⬅️":
+            # Previous page
+            if self.page_index > 0:
+                self.page_index -= 1
+        elif emoji == "➡️":
+            # Next page
+            if self.page_index < len(self.pages) - 1:
+                self.page_index += 1
+        elif emoji.endswith("\u20e3"):
+            # Option selection
+            index = int(emoji[0]) - 1  # Convert emoji number to index
+            option_index = index + self.page_index * self.options_per_page
+            if 0 <= option_index < len(self.current_options):
+                self.selections.append(self.current_options[option_index])
+                self.current_level += 1
+
+        options = self.get_options()  # Fetch updated options
+        await self.send_menu(menu_message, options, update=True)  # Pass the updated options
+
+    async def send_menu(self, message, options, update=False):
+        if not update:
+            self.page_index = 0
+        await self.show_page(message, update)
+
+    async def show_page(self, message, update=False):
+        degree = "\u00b0"
+        options = self.get_options()
+        self.current_options = options  # Store the current options for pagination
+        self.pages = [options[i:i + self.options_per_page] for i in range(0, len(options), self.options_per_page)]
+
+        if len(options) == 3 and "Degree" in options and "Chords" in options and "Notes" in options:
+
+            values = self.get_options(output =True)
+
+            # Create a custom embed for the specific case
+            myEmbed = discord.Embed(color=0x7e016f)
+            chord_name = self.selections[-1] if self.selections else "Unknown Chords"
+            myEmbed.set_author(name=f"{chord_name} Chords", icon_url=message.guild.icon.url)
+            
+            # Fetch values for Degree, Chords, and Notes from your JSON data
+            degree_values = values['Degree']
+            chords_values = values['Chords']
+            notes_values = values['Notes']
+
+
+
+            # Add fields for Degree, Chords, and Notes based on your JSON data if values are not empty
+            if degree_values:
+                degree_values = degree_values.replace('{degree}','°' )
+                myEmbed.add_field(name="Degree", value=f"`{degree_values}`", inline=True)
+            if chords_values:
+                degree_values = degree_values.replace('{degree}','°' )
+                myEmbed.add_field(name="Chords", value=f"`{chords_values}`", inline=True)
+            if notes_values:
+                degree_values = degree_values.replace('{degree}','°' )
+                myEmbed.add_field(name="Notes", value=f"`{notes_values}`", inline=True)
+
+
+
+            if self.menu_message is None:
+                self.menu_message = await message.channel.send(embed=myEmbed)
+            else:
+                await self.menu_message.edit(embed=myEmbed)
+
         else:
-            # Show sub-category selection if a category is selected
-            subcategories = options.get(self.category, {}).get("Categories", {})
-            embed = discord.Embed(title="Select a subcategory:", color=0x7e016f)
-            for i, subcategory in enumerate(subcategories, start=1):
-                embed.add_field(name=f"{i}. {subcategory}", value=f"Select {subcategory}", inline=False)
-            embed.set_footer(text="React with the corresponding number to select a subcategory.")
-            return await channel.send(embed=embed)
+            # Create a standard embed
+            embed = discord.Embed(color=0x7e016f,
+                                 description="\n".join(f"{index + 1}. {option}" for index, option in enumerate(self.pages[self.page_index]))
+                                  )
+            chord_name = self.selections[-1] if self.selections else "Menu"
+            embed.set_author(name=f"{chord_name}", icon_url=message.guild.icon.url)
 
-    async def send_menu(self, ctx, page):
-        if not self.subcategory:
-            subcategories = options.get(self.category, {}).get("Categories", {})
-            subcategory_names = list(subcategories.keys())
-            subcategory_name = subcategory_names[page]
-            subcategory_data = subcategories.get(subcategory_name, {})
-            embed = discord.Embed(title=f"Select a {subcategory_name}:", color=0x7e016f)
-            for key, value in subcategory_data.items():
-                embed.add_field(name=key, value=value, inline=False)
-            embed.set_footer(text=f"Page {page + 1}/{len(subcategory_names)}")
-            return await ctx.send(embed=embed)
+            if self.page_index > 0:
+                embed.set_footer(text="Page {} of {}".format(self.page_index + 1, len(self.pages)))
 
-    @menus.button('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f')
-    async def on_arrow_left(self, payload):
-        if self.current_page > 0:
-            self.current_page -= 1
-            await self.show_page(self.current_page)
+            if self.current_level > 0:
+                embed.add_field(name="Go Back", value="↩️", inline=False)
 
-    @menus.button('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f')
-    async def on_arrow_right(self, payload):
-        subcategories = options.get(self.category, {}).get("Categories", {})
-        if self.current_page < len(subcategories) - 1:
-            self.current_page += 1
-            await self.show_page(self.current_page)
+            if self.menu_message is None:
+                self.menu_message = await message.channel.send(embed=embed)
+            else:
+                await self.menu_message.edit(embed=embed)
 
-    @menus.button('\N{CROSS MARK}\ufe0f')
-    async def on_stop(self, payload):
-        self.stop()
+        if self.current_level > 0:
+            await self.menu_message.add_reaction("↩️")  # Always add "Go Back" reaction
 
-    def reaction_check(self, payload):
-        return payload.user_id == self.message.author.id
+        if len(options) > 1 and not (len(options) == 3 and "Degree" in options and "Chords" in options and "Notes" in options):
+            for i in range(len(self.pages[self.page_index])):
+                emoji_label = f"{i + 1}\u20e3"
+                await self.menu_message.add_reaction(emoji_label)
+
+        if self.page_index > 0:
+            await self.menu_message.add_reaction("⬅️")  # Left arrow (only if there's a previous page)
+        if len(self.pages) > 1 and self.page_index < len(self.pages) - 1:
+            await self.menu_message.add_reaction("➡️")  # Right arrow (only if there's a next page)
 
 
+    def get_options(self, output= False):
+        options = []
 
-#The Cog
+        data = self.json_data
+        for i in range(self.current_level):
+            selection = self.selections[i]
+            if selection in data:
+                data = data[selection]
+            else:
+                return options
+
+        if output:
+            return data
+
+        if isinstance(data, dict):
+            options = list(data.keys())
+        elif isinstance(data, str):
+            options.append(data)
+
+        # Check if a chord is selected and return all three options
+        if self.current_level == 1 and options == ["Degree"]:
+            options.extend(["Chords", "Notes"])
+
+        return options
+
+    async def send_data(self, ctx, data):
+        embed = discord.Embed(description=data)
+        await ctx.send(embed=embed)
+
+    async def start(self, ctx):
+        options = self.get_options()
+        self.current_options = options  # Store the current options for pagination
+        await self.send_menu(ctx, options)
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def notes(self, ctx):
-        menu = NotesMenu()
+        with open("cogs/options.json", "r") as file:
+            json_data = json.load(file)
+
+        menu = NotesMenu(ctx, self.bot, json_data)
         await menu.start(ctx)
 
 async def setup(bot):
