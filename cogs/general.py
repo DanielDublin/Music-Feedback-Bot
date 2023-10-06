c1f5f7eb9145e6615469ae639b858b8e645938c5
Daniel Dublin <bots4fun1@gmail.com>
2023-10-07 01:02:54 +0300

    changed the function signatures of MFR and MFS deleted extra unneeded  code

--- cogs/general.py
+++ cogs/general.py
@@ -7,7 +7,7 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    #note: need to add functions in db of fetch_points, fetch_rank
 
     #MF balance 
     @commands.command()             
@@ -56,67 +56,48 @@ class General(commands.Cog):
             await send(embed = embed)            
 
 
-        #Add points        
-    async def add_points_to_json(users, user, message):
-        if message.content.startswith("<MFR") or message.content.startswith("<mfr"):
-            users[f"{user.id}"]["points"] += 1
-            points = users[f"{user.id}"]["points"]
-            mention = message.author.mention
-            await message.channel.send(f"{mention} has gained 1 MF point. You now have **{points}** MF point(s).", delete_after = 4) 
-            channel = client.get_channel(749443325530079314)
-            my_ID = "412733389196623879"
-            myEmbed = discord.Embed(color = 0x7e016f)
-            myEmbed.add_field(name = "Feedback Notice", value = f"{mention} has **given feedback** and now has **{points}** MF point(s).", inline = False)
-            await channel.send(embed = myEmbed)        
+        #Add points 
+    @commands.command(name = "R")       
+    async def MFR_command(self, ctx : discord.Message):
+        users[f"{user.id}"]["points"] += 1
+        points = users[f"{user.id}"]["points"]
+        mention = message.author.mention
+        await message.channel.send(f"{mention} has gained 1 MF point. You now have **{points}** MF point(s).", delete_after = 4) 
+        channel = client.get_channel(749443325530079314)
+        my_ID = "412733389196623879"
+        myEmbed = discord.Embed(color = 0x7e016f)
+        myEmbed.add_field(name = "Feedback Notice", value = f"{mention} has **given feedback** and now has **{points}** MF point(s).", inline = False)
+        await channel.send(embed = myEmbed)        
 
 
     #Use points        
-    async def use_points_from_json(users, user, message):     
-        if message.content.startswith("<MFS") or message.content.startswith("<mfs"):
-            users[f"{user.id}"]["points"] -= 1
-            points = users[f"{user.id}"]["points"]
-            mention = message.author.mention
+    @commands.command(name = "S")       
+    async def MFs_command(self, ctx : discord.Message):     
+        users[f"{user.id}"]["points"] -= 1
+        points = users[f"{user.id}"]["points"]
+        mention = message.author.mention
+        if users[f"{user.id}"]["points"] <= -1:
+            await message.channel.send(f"{mention}, you do not have any MF points. Please give feedback first.", delete_after = 5)  
+            await message.delete()
+            channel = client.get_channel(749443325530079314)
+            my_ID = "412733389196623879"
+            await channel.send(f"<@{my_ID}>:") 
+            myEmbed = discord.Embed(color = 0x7e016f)
+            myEmbed.add_field(name = "**ALERT**", value = f"{mention} tried sending a track for feedback with **0** MF points.", inline = False) 
+            await channel.send(embed = myEmbed)
             if users[f"{user.id}"]["points"] <= -1:
-                await message.channel.send(f"{mention}, you do not have any MF points. Please give feedback first.", delete_after = 5)  
-                await message.delete()
-                channel = client.get_channel(749443325530079314)
-                my_ID = "412733389196623879"
-                await channel.send(f"<@{my_ID}>:") 
-                myEmbed = discord.Embed(color = 0x7e016f)
-                myEmbed.add_field(name = "**ALERT**", value = f"{mention} tried sending a track for feedback with **0** MF points.", inline = False) 
-                await channel.send(embed = myEmbed)
-                if users[f"{user.id}"]["points"] <= -1:
-                    users[f"{user.id}"]["points"] += 1
+                users[f"{user.id}"]["points"] += 1
                 
-            elif users[f"{user.id}"]["points"] >= 0:
-                await message.channel.send(f"{mention} have used 1 MF point. You now have **{points}** MF point(s).", delete_after = 4)
-                channel = client.get_channel(749443325530079314)
-                my_ID = "412733389196623879"
-                myEmbed = discord.Embed(color = 0x7e016f)
-                myEmbed.add_field(name = "Feedback Notice", value = f"{mention} has **submitted** a work for feedback and now has **{points}** MF point(s).", inline = False)
-                await channel.send(embed = myEmbed)        
-
-                
+        elif users[f"{user.id}"]["points"] >= 0:
+            await message.channel.send(f"{mention} have used 1 MF point. You now have **{points}** MF point(s).", delete_after = 4)
+            channel = client.get_channel(749443325530079314)
+            my_ID = "412733389196623879"
+            myEmbed = discord.Embed(color = 0x7e016f)
+            myEmbed.add_field(name = "Feedback Notice", value = f"{mention} has **submitted** a work for feedback and now has **{points}** MF point(s).", inline = False)
+            await channel.send(embed = myEmbed)        
 
 
-
-
-    @commands.command()
-    async def add_points(self, ctx, points: int):
-        """Add points to a user."""
-        pool = await init_database()
-        async with pool.acquire() as conn:
-            async with conn.cursor() as cursor:
-                user_id = ctx.author.id
-                await cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
-                user = await cursor.fetchone()
-                if user:
-                    new_points = user['points'] + points
-                    await cursor.execute("UPDATE users SET points = %s WHERE id = %s", (new_points, user_id))
-                else:
-                    await cursor.execute("INSERT INTO users (id, name, points) VALUES (%s, %s, %s)", (user_id, ctx.author.display_name, points))
-
-        await ctx.send(f"You now have {new_points} points!")
+ 
 
 async def setup(bot):
     await bot.add_cog(General(bot))
