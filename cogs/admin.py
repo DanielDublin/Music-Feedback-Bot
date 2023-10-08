0980fb8f03e8f083a92136f997f5bb2c94ae41c2
Daniel Dublin <bots4fun1@gmail.com>
2023-10-09 00:11:50 +0300

    re-written balance command added a comment about it having no perm requirements

--- cogs/admin.py
+++ cogs/admin.py
@@ -51,20 +51,16 @@ class Admin(commands.Cog):
 
         
      
-    #Mod balance        
-    @commands.command()
+    #Mod balance        WHY U NO PERMS?!
+    @commands.command(name = "balance", aliases = [" balance"])
+    @commands.has_permissions()
     async def balance(self, ctx :discord.Message, user: discord.Member):
     
+        points = await db.fetch_points(user.id)
+        embed = discord.Embed(color = 0x7e016f)
+        embed.add_field(name = "Music Feedback", value = f"{user.mention} has **{points}** MF point(s).", inline = False)
+        await ctx.channel.send(embed = embed)             
        
-        
-            if str(user.id) in users:
-                points = users[f"{user.id}"]["points"]
-                myEmbed = discord.Embed(color = 0x7e016f)
-                myEmbed.add_field(name = "Music Feedback", value = f"{user.mention} has **{points}** MF point(s).", inline = False)
-                await ctx.send(embed = myEmbed)             
-         
-        with open("MF Points.json", "w") as f:
-            json.dump(users, f,indent = 4)        
         
 
 
