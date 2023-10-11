444e168276c81dc21c54408588104a226866211f
Daniel Dublin <bots4fun1@gmail.com>
2023-10-11 22:09:20 +0300

    fixed indents, removed uncessary commas

--- database/db.py
+++ database/db.py
@@ -7,7 +7,6 @@ load_dotenv()
 USER_ID = 0
 POINTS = 1
 WARNINGS = 2
-SUCCESS = 100
 DATABASE_ERROR = -2
 
 
@@ -53,10 +52,12 @@ async def schedule_weekly_task():
 # Initialize the database connection pool (you should do this before using the functions)
 async def initialize_database_pool():
     global pool
+    
     pool = await aiomysql.create_pool(host='your_host', user='your_user', password='your_password', db='your_database')
 
 async def update_dict_from_db(self, user_id):
-    global pool, users_dict, USER_ID, POINTS, WARNINGS, SUCCESS
+    global pool, users_dict, POINTS, WARNINGS
+    
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
             
@@ -73,6 +74,7 @@ async def update_dict_from_db(self, user_id):
                 
 async def fetch_rank_from_db(self, user_id):
     global pool
+    
     async with pool.acquire() as conn:
             async with conn.cursor() as cursor:
                 # Fetch rank from the database
@@ -80,18 +82,19 @@ async def fetch_rank_from_db(self, user_id):
                     SELECT COUNT(*) + 1
                     FROM users
                     WHERE points > (SELECT points FROM users WHERE user_id = %s)
-                """, (str(user_id),))  # Convert to str
-                result = await cursor.fetchone()
-                return result
+                """, (str(user_id)))  # Convert to str
+    result = await cursor.fetchone()
+    return result
 
 # Fetch points for a user
 async def fetch_points(user_id: int):
     global users_dict
+    
     if user_id in users_dict:
         return users_dict[user_id]["Points"]  # Return points from the dictionary
     else:
-            await update_dict_from_db(user_id)
-            return users_dict[user_id]["Points"]
+        await update_dict_from_db(user_id)
+        return users_dict[user_id]["Points"]
       
 
 # Fetch rank for a user
@@ -117,7 +120,7 @@ async def fetch_rank(user_id: int):
 
 # Fetch the top 5 users with the best point scores
 async def fetch_top_users():
-    global pool, USER_ID, POINTS
+    global pool, users_dict, USER_ID, POINTS
     
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
@@ -128,26 +131,30 @@ async def fetch_top_users():
                 ORDER BY points DESC
                 LIMIT 5
             """)
-            top_users = await cursor.fetchall()
-            top_users_dict = {user[USER_ID]: str(user[POINTS]) for user in top_users}
-            return top_users_dict
+    top_users = await cursor.fetchall()
+    top_users_dict = {user[USER_ID]: str(user[POINTS]) for user in top_users}
+    return top_users_dict
         
 
 
 # Reduce points for a user
 async def reduce_points(user_id: int, points: int):
+    global pool, users_dict
+    
     if user_id in users_dict:
         users_dict[user_id][0] -= points  # Reduce points in the dictionary        
    
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
             # Reduce points in the database
-            await cursor.execute("UPDATE users SET points = points - %s WHERE user_id = %s", (points, str(user_id)))  # Convert to str
+            await cursor.execute("UPDATE users SET points = (points - %s) WHERE user_id = %s", (points, str(user_id)))  # Convert to str
             await conn.commit()
            
 
 # Add points for a user
 async def add_points(user_id: int, points: int):
+    global pool, users_dict
+    
     if user_id in users_dict:
         users_dict[user_id][0] += points  # Add points in the dictionary
     else:
@@ -160,41 +167,50 @@ async def add_points(user_id: int, points: int):
 
 # Add a user
 async def add_user(user_id: int):
-    global pool
+    global pool, users_dict
+    
     if user_id not in users_dict:
         async with pool.acquire() as conn:
             async with conn.cursor() as cursor:
                 # Add or replace user in the database (use "REPLACE INTO" or "INSERT INTO ON DUPLICATE KEY UPDATE" depending on your database)
-                await cursor.execute("REPLACE INTO users (user_id, points) VALUES (%s, 0)", (str(user_id),))  # Convert to str
+                await cursor.execute("REPLACE INTO users (user_id) VALUES (%s)", (str(user_id)))  # Convert to str
                 await conn.commit()
-                user_data = {"Points": 0, "Warnings": 0}
-                users_dict[user_id] = user_data  # Add user to the dictionary
+        user_data = {"Points": 0, "Warnings": 0}
+        users_dict[user_id] = user_data  # Add user to the dictionary
 
 # Remove a user
 async def remove_user(user_id: int):
+    global pool, users_dict
+    
     if user_id in users_dict:
         del users_dict[user_id]  # Remove user from the dictionary
+        
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
             # Send SQL query to remove the user from the database
-            await cursor.execute("DELETE FROM users WHERE user_id = %s", (str(user_id),))  # Convert to str
+            await cursor.execute("DELETE FROM users WHERE user_id = %s", (str(user_id)))  # Convert to str
             await conn.commit()
 
 # Reset points for a user
 async def reset_points(user_id: int):
+    global pool, users_dict
+    
     if user_id in users_dict:
-        users_dict[user_id][0] = 0  # Reset points in the dictionary
+        users_dict[user_id]["Points"] = 0  # Reset points in the dictionary
+        
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
             # Send SQL query to reset points for the user in the database
-            await cursor.execute("UPDATE users SET points = 0 WHERE user_id = %s", (str(user_id),))  # Convert to str
+            await cursor.execute("UPDATE users SET points = 0 WHERE user_id = %s", (str(user_id)))  # Convert to str
             await conn.commit()
     
 
 async def json_migration(users):
+    global pool
+    
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
-            # Create a batch to accumulate SQL commands
+            # The batch to accumulate SQL commands
             batch = []
             
             for user_id, user_data in users.items():
@@ -204,12 +220,11 @@ async def json_migration(users):
                 # If the batch size reaches 500 or at the end of the loop, execute the REPLACE command
                 if len(batch) == 500 or user_id == list(users.keys())[-1]:
                     placeholders = ",".join(["(%s, %s, 0)"] * len(batch))
-                    sql = f"REPLACE INTO users (user_id, points, warnings) VALUES {placeholders}"
+                    sql = f"REPLACE INTO users (user_id, points, warnings) VALUES {placeholders}; "
                     values = [item for sublist in batch for item in sublist]
                     
                     await cursor.execute(sql, values)
-                    await conn.commit()
-                    
+                    await conn.commit()     
                     
                     # Clear the batch for the next set of data
                     batch = []
