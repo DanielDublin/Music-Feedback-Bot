5748e1955aa4e41a98505a9eb096f0922c9b6ff4
Daniel Dublin <bots4fun1@gmail.com>
2023-10-12 01:18:48 +0300

    added exceptions to handler, fixed db.py, changed the extraction of data from result (output from db) re-did fetch_top_users to return a rank as well and changed top command to match it

--- exception_handler.py
+++ exception_handler.py
@@ -3,16 +3,25 @@ import discord
 # Define a function to handle exceptions
 async def handle_exception(ctx, error):
     # Get the cog name if available
-    cog_name = getattr(ctx.command.cog, "qualified_name", "Unknown Cog")
+    try:
+        cog_name = getattr(ctx.command.cog, "qualified_name", "Unknown Cog")
+    except Exception as cog_error:
+        print("someone tried to use a command that doesnt exist")
 
-    if isinstance(error, discord.ext.commands.CommandNotFound):
-        await ctx.send(f"Command not found in the '{cog_name}' cog. Check your command syntax.")
-    elif isinstance(error, discord.ext.commands.MissingPermissions):
-        await ctx.send(f"You don't have permission to use a command in the '{cog_name}' cog.")
-    elif isinstance(error, discord.ext.commands.CheckFailure):
-        await ctx.send(f"You don't meet the requirements to use a command in the '{cog_name}' cog.")
-    elif isinstance(error, discord.ext.commands.BadArgument):
-        await ctx.send(f"Invalid argument in the '{cog_name}' cog. Please check your input.")
-    else:
-        # Handle other unexpected errors
-        await ctx.send(f"An error occurred in the '{cog_name}' cog: {error}")
+        try:
+                await ctx.send(f"No such command exists.")
+        except Exception as e:
+            print("No permissions to send message\n"+str(e))
+
+    try:
+        if isinstance(error, discord.ext.commands.MissingPermissions):
+            await ctx.send(f"You don't have permission to use a command in the '{cog_name}' cog.")
+        elif isinstance(error, discord.ext.commands.CheckFailure):
+            await ctx.send(f"You don't meet the requirements to use a command in the '{cog_name}' cog.")
+        elif isinstance(error, discord.ext.commands.BadArgument):
+            await ctx.send(f"Invalid argument in the '{cog_name}' cog. Please check your input.")
+        else:
+            # Handle other unexpected errors
+            await ctx.send(f"An error occurred in the '{cog_name}' cog: {error}")
+    except Exception as e:
+        print(f"ERROR IN HANDLE EXCEPTION from cog {cog_name}\n{str(e)}")
