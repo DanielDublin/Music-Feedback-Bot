1a24bc926bd9b65bf3faac0589241ab2c1549605
Daniel Dublin <bots4fun1@gmail.com>
2023-10-12 01:43:08 +0300

    changed id to str in admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -15,8 +15,8 @@ class Admin(commands.Cog):
     @commands.has_permissions(administrator = True)
     async def add(self, ctx :discord.Message, user :discord.Member, points :int = 1):
         
-        await db.add_points(user.id, points)
-        current_points = await db.fetch_points(user.id)
+        await db.add_points(str(user.id), points)
+        current_points = await db.fetch_points(str(user.id))
         embed = discord.Embed(color = 0x7e016f)
         embed.add_field(name = "Music Feedback", value = f"You have given {user.mention} {points} MF point. They now have **{points}** MF point(s).", inline = False)
         await ctx.send(embed = embed)                       
@@ -27,10 +27,10 @@ class Admin(commands.Cog):
     @commands.command(name = "remove", aliases = [" remove"])
     @commands.has_permissions(administrator = True)
     async def remove(self, ctx :discord.Message, user :discord.Member, points :int = 1):
-        current_points = await db.fetch_points(user.id)
+        current_points = await db.fetch_points(str(user.id))
         
         if current_points-points:
-            await db.reduce_points(user.id, points)       
+            await db.reduce_points(str(user.id), points)       
             embed = discord.Embed(color = 0x7e016f)
             embed.add_field(name = "Music Feedback", value = f"You have taken {points} MF point from {user.mention}. They now have **{current_points - points}** MF point(s).", inline = False)
             await ctx.channel.send(embed = embed) 
@@ -47,7 +47,7 @@ class Admin(commands.Cog):
     @commands.has_permissions(administrator = True)
     async def clear(self, ctx :discord.Message, user: discord.Member):
     
-        await db.reset_points(user.id)   
+        await db.reset_points(str(user.id))
         myEmbed = discord.Embed(color = 0x7e016f)
         myEmbed.add_field(name = "Music Feedback", value = f"{user.mention} has lost all their MF points. They now have **0** MF points.", inline = False)
         await ctx.channel.send(embed = myEmbed)             
