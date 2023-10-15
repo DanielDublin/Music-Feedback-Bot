6990ec6023306bd3ace10da8700aab9abd6ed5b7
Daniel Dublin <bots4fun1@gmail.com>
2023-10-16 00:16:59 +0300

    fixed various stuff, discord disabled reading about me so thats out the window

--- modules/youtube_promotion_checker.py
+++ modules/youtube_promotion_checker.py
@@ -1,31 +1,41 @@
 import re
 import os
 import discord
-import youtube_dl
+import yt_dlp as youtube_dl
+import urlextract
 from dotenv import load_dotenv
 
 load_dotenv()
 YOUTUBE_API_KEY = os.environ.get('YOUTUBE_TOKEN')
 
-youtube_dl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
+# youtube_dl options
+ydl_opts = {
+    'outtmpl': '%(id)s.%(ext)s',
+    'quiet': True,  # Optional: Set this to True to suppress output messages
 }
 
-# Regular expressions to match different YouTube URL formats
-youtube_url_pattern = re.compile(r'(https?://)?(www\.)?(youtube\.com|youtu\.be)/(.+)')
+# Regular expression to match YouTube URLs and extract video IDs
+youtube_url_pattern = re.compile(r'(?:https?://)?(?:www\.)?(?:(?:youtube\.com/embed/|youtube\.com/v/|youtube\.com/watch\?v=|youtube\.com/attribution_link\?a=|youtu\.be/)([\w-]+)|(?:youtube\.com/shorts/|youtube\.com/playlist\?list=)([A-Za-z0-9_-]+))(?:&\S*)?$')
+    # Initialize the URL extractor
+url_extractor = urlextract.URLExtract()
 
-
-# Function to extract the video ID from a YouTube content
-def extract_youtube_video_id(content):
-    match = youtube_url_pattern.match(content)
+# Function to extract the first video ID from a text
+def extract_youtube_video_id(text):
+    match = youtube_url_pattern.search(text)
     if match:
-        return match.group(4)
+        return match.group(1) or match.group(2)
     return None
+
+
+# Function to extract URLs from text
+def extract_urls(content):
+    out_links = []
+    urls = list(url_extractor.find_urls(content))
+    for url in urls:
+        if 'youtu' in url:
+            out_links.append(url)
+    return out_links
+        
 
 
 async def check_youtube(message):
@@ -33,32 +43,39 @@ async def check_youtube(message):
     content = message.content
     
     # Extract YouTube video ID from the URL
-    youtube_video_id = extract_youtube_video_id(content)
-    if youtube_video_id is None:
+    youtube_links = extract_urls(message.content)
+    if youtube_links is None:
         return False
+    
+    for link in youtube_links:
+        youtube_video_id = extract_youtube_video_id(content)
+        if youtube_video_id is None:
+            continue
 
-    try:
-        # Get the author's profile
-        profile = await message.author.fetch()
+        try:
+            # Get the author's profile
+            profile = message.author
 
-        # Get the YouTube video's details
-        ydl = youtube_dl.YoutubeDL(youtube_dl_opts)
-        with ydl:
-            info = ydl.extract_info(f'https://www.youtube.com/watch?v={youtube_video_id}', download=False)
-            creator = info.get('creator')
-
-        # Check if the author's username or display name contains the YouTube creator's name
-        if creator and (creator in profile.name or creator in profile.display_name or profile.bio):
-            return True
-        else:
-
-            # Check if the link to YouTube is in the user's connections
-            creator_in_youtube_connections = any(
-                    creator in connection.get('name', '') and connection.get('type') == discord.ConnectionType.youtube for connection in profile.connections
-                )
-            if  creator_in_youtube_connections:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(f'https://www.youtube.com/watch?v={youtube_video_id}', download=False)
+                creator = info.get('uploader')
+            
+            # Check if the author's username or display name contains the YouTube creator's name
+            if creator and (creator.lower() == profile.global_name.lower() or creator.lower() == profile.display_name.lower()):
                 return True
             else:
-                return False
-    except discord.errors.NotFound:
-        print("Unable to fetch the user's profile.")
+
+                # Check if the link to YouTube is in the user's connections
+                creator_in_youtube_connections = any(
+                        creator in connection.get('name', '') and connection.get('type') == discord.ConnectionType.youtube for connection in profile.connections
+                    )
+                if  creator_in_youtube_connections:
+                    return True
+                else:
+                    continue
+        except discord.errors.NotFound:
+            print("Unable to fetch the user's profile.")
+        except Exception as e:
+            print(str(e))
+            
+    return False
