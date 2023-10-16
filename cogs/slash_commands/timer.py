90b3f4dfbbcbeb37674c74bc678d8df62f64b911
Daniel Dublin <bots4fun1@gmail.com>
2023-10-17 02:49:32 +0300

    made admin.py into a slash commands module, moved the migration commansd to a new cog module, made timer.py changed bot.py accordingly

---
+++ cogs/slash_commands/timer.py
@@ -0,0 +1,68 @@
+import asyncio
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+from data.constants import SERVER_ID
+
+class TimerCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.active_timer = None  
+    
+        
+        
+    group = app_commands.Group(name="timer", description="Countdown from any time (minutes)", default_permissions=discord.Permissions(administrator=True))
+    
+
+    @group.command(name = "status") 
+    async def status(self, interaction):
+        if self.active_timer is not None:
+            await interaction.response.send_message(f"A timer is already running. Use `{self.bot.command_prefix}timer stop` to stop it.", ephemeral=True)
+        else:
+            await interaction.response.send_message(f"Use `{self.bot.command_prefix}timer start <minutes>` to start a timer.", ephemeral=True)
+
+    async def timer_countdown(self, interaction, minutes):
+        
+        channel = interaction.channel
+        await channel.send(f"Timer is starting, _wait for it..._ **GO!**")
+        counter =0
+        timers =  [5, 4, 3, 2, 1]
+        
+        while counter != len(timers):
+            if minutes in timers:
+                counter +=1
+                await channel.send(f"{minutes} minutes remaining.")
+                
+            await asyncio.sleep(60)
+            minutes -=1
+                
+        await asyncio.sleep(60)
+        await channel.send("Time is up!")
+        self.active_timer = None
+        
+
+    @group.command(name="start")
+    async def start(self, interaction: discord.Interaction, minutes :int) -> None:
+        author_id = interaction.user.id
+        
+        if self.active_timer is not None:
+            await interaction.response.send_message(f"A timer is already running. Use `{self.bot.command_prefix}timer stop` to stop it.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Starting the timer.", ephemeral=True)
+            self.active_timer = asyncio.create_task(self.timer_countdown(interaction, minutes))
+            
+
+    @group.command(name="stop")
+    async def stop(self, interaction: discord.Interaction) -> None:
+        author_id = interaction.user.id
+        
+        if self.active_timer is not None:
+            self.active_timer.cancel()
+            await interaction.response.send_message("Timer stopped.", ephemeral=True)
+            self.active_timer = None
+        else:
+            await interaction.response.send_message("No timer is currently running.", ephemeral=True)
+
+
+async def setup(bot):
+    await bot.add_cog(TimerCog(bot), guild=discord.Object(id=SERVER_ID))
