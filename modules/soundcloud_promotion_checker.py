5a23ce2f7a7a897a46b1a7038a9d51b989408fef
Daniel Dublin <bots4fun1@gmail.com>
2023-10-16 04:56:52 +0300

    made everything to work

--- modules/soundcloud_promotion_checker.py
+++ modules/soundcloud_promotion_checker.py
@@ -2,11 +2,25 @@ import re
 import aiohttp
 import discord
 import urlextract
+from bs4 import BeautifulSoup
 
 # Define a regular expression pattern to match SoundCloud URLs
 soundcloud_url_pattern = re.compile(r'https?://soundcloud\.com/([A-Za-z0-9_-]+)')
 # Initialize the URL extractor
 url_extractor = urlextract.URLExtract()
+
+
+
+def extract_soundcloud_url(message_content):
+    # Function to extract URLs from text
+    out_links = []
+    urls = list(url_extractor.find_urls(message_content))
+    for url in urls:
+        if 'soundcloud' in url:
+            out_links.append(url)
+    return out_links
+        
+
 
 async def expand_soundcloud_url(short_url):
     async with aiohttp.ClientSession() as session:
@@ -14,9 +28,10 @@ async def expand_soundcloud_url(short_url):
             if response.status == 200:
                 # Extract the full URL from the response headers
                 full_url = response.url
+                full_url = str(full_url).strip("URL('')")
                 
                 # Check if the full URL is a SoundCloud URL
-                if re.match(r'https?://(www\.)?soundcloud\.com/([A-Za-z0-9_-]+)', str(full_url)):
+                if re.match(r'https?://(www\.)?soundcloud\.com/([A-Za-z0-9_-]+)', full_url):
                     return full_url
                 else:
                     return None
@@ -35,22 +50,53 @@ def extract_soundcloud_channel_name(expanded_url):
     else:
         return None
     
-def extract_soundcloud_url(message_content):
-    # Function to extract URLs from text
-    out_links = []
-    urls = list(url_extractor.find_urls(message_content))
-    for url in urls:
-        if 'soundcloud' in url:
-            out_links.append(url)
-    return out_links
-        
+def extract_soundcloud_song_name(expanded_url):
+    # Regular expression to match the channel name in a SoundCloud URL
+    soundcloud_url_pattern = re.compile(r'https?://soundcloud\.com/[\w-]+/([\w-]*)')
+
+    match = soundcloud_url_pattern.match(expanded_url)
+
+    if match:
+        song_name = match.group(1)
+        return song_name
+    else:
+        return None
+   
+
+async def fetch_soundcloud_display_name(expanded_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(expanded_url) as response:
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
+                title_element = soup.select_one('title')
+                if title_element:
+                    # Extract the content of the <title> tag
+                    title_content = title_element.text
+                    
+                    song_name = extract_soundcloud_song_name(expanded_url)
+                    display_name_match = None
+                    
+                    if song_name is not None: # We are at the main channel
+                        display_name_match = re.search(r'by (.*?) \|', title_content)
+                    else:
+                        display_name_match = re.search(r'Stream (.+?) music \|', title_content)
+
+                    if display_name_match is not None:
+                        display_name = display_name_match.group(1)
+                        return display_name
+                    else:
+                        return None
+                else:
+                    return None
+                
 
 
 async def check_soundcloud(message):
     content =message.content
   
     short_urls = extract_soundcloud_url(content)
-    if short_url is None:
+    if short_urls is None:
         return False
     
     for url in short_urls:
@@ -59,18 +105,33 @@ async def check_soundcloud(message):
             print("error while expanding url from soundcloud")
             continue
     
-        soundcloud_user = extract_soundcloud_channel_name(expanded_url)
-        if soundcloud_user is None:
+        soundcloud_default_user_name =  extract_soundcloud_channel_name(expanded_url)
+        if soundcloud_default_user_name is None:
             print("error while getting username from soundcloud")
             continue
-        
-        soundcloud_user = soundcloud_user.lower()
-        try:
-            author = message.author
-            # Check if the the Soundcloud username is in the name, nickname or bio of the discord's user
-            if soundcloud_user in author.global_name.replace(" ","").lower() or soundcloud_user in author.display_name.replace(" ","").lower():
-                return True
 
+
+        soundcloud_user_display_name = await fetch_soundcloud_display_name(expanded_url)
+        if soundcloud_user_display_name is None:
+            print("error while getting display name from soundcloud")
+            continue
+        
+   
+        author = message.author
+        soundcloud_user_display_name = soundcloud_user_display_name.replace(" ","").replace("-","").replace("_","").lower()
+        soundcloud_default_user_name = soundcloud_default_user_name.replace(" ","").replace("-","").replace("_","").lower()
+        discord_global_name = author.global_name.replace(" ","").replace("-","").replace("_","").lower()
+        discord_display_name = author.display_name.replace(" ","").replace("-","").replace("_","").lower()
+        
+        try:     
+            # Check if the the Soundcloud username is in the name, nickname or bio of the discord's user
+            if soundcloud_user_display_name == discord_global_name or soundcloud_user_display_name == discord_display_name:
+                return True
+            elif soundcloud_default_user_name == discord_global_name or soundcloud_default_user_name == discord_display_name:
+                return True
+            
         except discord.errors.NotFound:
             print(f"Unable to fetch the user's profile.")
+            
+    return False
         
--- modules/youtube_promotion_checker.py
+++ modules/youtube_promotion_checker.py
@@ -14,41 +14,54 @@ ydl_opts = {
     'quiet': True,  # Optional: Set this to True to suppress output messages
 }
 
-# Regular expression to match YouTube URLs and extract video IDs
-youtube_url_pattern = re.compile(r'(?:https?://)?(?:www\.)?(?:(?:youtube\.com/embed/|youtube\.com/v/|youtube\.com/watch\?v=|youtube\.com/attribution_link\?a=|youtu\.be/)([\w-]+)|(?:youtube\.com/shorts/|youtube\.com/playlist\?list=)([A-Za-z0-9_-]+))(?:&\S*)?$')
+# YT URL patterns
+youtube_url_pattern1 = re.compile(r'(https?://)?(?:www\.)?youtube\.com/watch\?v=([A-Za-z0-9_-]+)&list=([A-Za-z0-9_-]+)')
+youtube_url_pattern2 = re.compile(r'(https?://)?(?:www\.)?youtube\.com/watch\?v=([A-Za-z0-9_-]+)')
+youtube_url_pattern3 = re.compile(r'(https?://)?youtu\.be/([A-Za-z0-9_-]+)(?:\?.*)?$')
+
+
 # Initialize the URL extractor
 url_extractor = urlextract.URLExtract()
 
-# Function to extract the first video ID from a text
-def extract_youtube_video_id(text):
-    match = youtube_url_pattern.search(text)
-    if match:
-        return match.group(1) or match.group(2)
+
+# Function to extract the video ID from a YouTube URL
+def extract_youtube_video_id(url):
+    if youtube_url_pattern1.match(url.replace('\n',"")):
+        match = youtube_url_pattern1.search(url)
+        if match:
+            return match.group(2)
+    elif youtube_url_pattern2.match(url):
+        match = youtube_url_pattern2.search(url.replace('\n',""))
+        if match:
+            return match.group(2)
+    elif youtube_url_pattern3.match(url):
+        match = youtube_url_pattern3.search(url.replace('\n',""))
+        if match:
+            return match.group(2)
     return None
 
 
 # Function to extract URLs from text
-def extract_urls(content):
+def extract_video_urls(content):
     out_links = []
+    pattern = r'.*https?://'
+    
     urls = list(url_extractor.find_urls(content))
     for url in urls:
-        if 'youtu' in url:
-            out_links.append(url)
+        if 'youtu' in url and "channel" not in url and "@" not in url:
+            cleaned_url = re.sub(pattern, '', url)
+            out_links.append(cleaned_url)
     return out_links
-        
 
 
-async def check_youtube(message):
-    
-    content = message.content
-    
+async def handle_videos(message):
     # Extract YouTube video ID from the URL
-    youtube_links = extract_urls(message.content)
+    youtube_links = extract_video_urls(message.content)
     if youtube_links is None:
         return False
     
     for link in youtube_links:
-        youtube_video_id = extract_youtube_video_id(content)
+        youtube_video_id = extract_youtube_video_id(link)
         if youtube_video_id is None:
             continue
 
@@ -61,12 +74,82 @@ async def check_youtube(message):
                 creator = info.get('uploader')
             
             # Check if the author's username or display name contains the YouTube creator's name
-            if creator and (creator.lower() == profile.global_name.lower() or creator.lower() == profile.display_name.lower()):
+            discord_global_name = profile.global_name.replace(" ","").lower()
+            discord_display_name = profile.display_name.replace(" ","").lower()
+            creator = creator.replace(" ","").lower()
+            if creator and (creator.lower() == discord_global_name or creator.lower() == discord_display_name):
                 return True
     
         except discord.errors.NotFound:
             print("Unable to fetch the user's profile.")
         except Exception as e:
             print(str(e))
-            
     return False
+
+
+# Function to extract URLs from text
+def extract_channel_urls(content):
+    out_links = []
+    pattern = r'.*https?://'
+    
+    urls = list(url_extractor.find_urls(content))
+    for url in urls:
+        if 'youtu' in url and "@" in url:
+            cleaned_url = re.sub(pattern, '', url)
+            out_links.append(cleaned_url)
+    return out_links
+        
+def get_youtube_channel_info(channel_url):
+    pattern = r'\.com/(@[\w-]+)'
+    # Use re.search to find the match
+    match = re.search(pattern, channel_url)
+    # Extract the username if a match is found
+    if match:
+        username = match.group(1)[1:] 
+        return username
+    return None
+      
+    
+
+async def handle_channels(message):
+        # Extract YouTube video ID from the URL
+    youtube_links = extract_channel_urls(message.content)
+    if youtube_links is None:
+        return False
+    
+    for link in youtube_links:
+    
+            try:
+                # Get the author's profile
+                profile = message.author
+
+                creator = get_youtube_channel_info(link)
+                if creator is None:
+                    continue
+            
+                # Check if the author's username or display name contains the YouTube creator's name
+                discord_global_name = profile.global_name.replace(" ","").lower()
+                discord_display_name = profile.display_name.replace(" ","").lower()
+                creator = creator.replace(" ","").lower()
+                if creator.lower() == discord_global_name or creator.lower() == discord_display_name:
+                    return True
+    
+            except discord.errors.NotFound:
+                print("Unable to fetch the user's profile.")
+            except Exception as e:
+                print(str(e))
+    return False
+
+
+async def check_youtube(message):
+    
+    content = message.content
+    
+    out = await handle_videos(message)
+    if out:
+        return True
+    else:
+        out = await handle_channels(message)
+    
+    
+    return out
