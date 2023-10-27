6fa43722259a1f9e98e067457ac0aa9c93cf0741
Daniel Dublin <bots4fun1@gmail.com>
2023-10-27 17:57:41 +0300

    added both options

--- cogs/help_command.py
+++ cogs/help_command.py
@@ -1,30 +1,53 @@
 ﻿from discord.ext import commands, menus
 import discord
+from discord import app_commands
+from datetime import datetime
 
 class HelpMenu(menus.Menu):
     def __init__(self, bot):
-        super().__init__(timeout=60.0, delete_message_after=True, clear_reactions_after=True)
+        super().__init__(timeout=180.0, delete_message_after=True, clear_reactions_after=True)
         self.user = None
         self.main_menu = None
         self.is_main = True
         self.bot = bot
         self.bot.add_listener(self.on_raw_reaction_add)
+        self.page_index =0
+        self.guild = None
+        self.message = None
         
     async def on_raw_reaction_add(self, payload):
-        if payload.user_id == self.user.id and payload.emoji.name == '↩️' and not self.is_main:
-            self.is_main = True
-            await self.message.clear_reactions()
-            await self.update_main_menu()
+        if payload.user_id == self.user.id:
+            if payload.emoji.name == '↩️' and not self.is_main:
+                self.is_main = True
+                self.page_index =0
+                await self.message.clear_reactions()
+                await self.update_main_menu()
+                
+            elif payload.emoji.name == "⬅️" and not self.is_main:
+                self.page_index -=1    
+                await self.message.clear_reaction('⬅️') 
+                await self.message.clear_reaction('➡️') 
+                await self.show_page(self.page_index)
+                
+            elif payload.emoji.name == "➡️" and not self.is_main:
+                self.page_index +=1
+                await self.message.clear_reaction('⬅️') 
+                await self.message.clear_reaction('➡️') 
+                await self.show_page(self.page_index)
+            
             
 
     async def send_initial_message(self, ctx, channel):
         self.user = ctx.author
+        self.guild = ctx.guild
         return await ctx.send(embed=self.get_main_menu())
 
     def get_main_menu(self):
         embed = discord.Embed(title="Help Menu", color=0x7e016f)
         embed.add_field(name="1️⃣ General", value="Show general commands", inline=False)
         embed.add_field(name="2️⃣ Admin", value="Show admin commands", inline=False)  
+        embed.set_footer(text=f"{self.guild.name}", icon_url=self.guild.icon.url)
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
         return embed
 
     async def update_main_menu(self):
@@ -38,14 +61,111 @@ class HelpMenu(menus.Menu):
         await self.message.clear_reactions()
         await self.message.add_reaction("↩️")
         self.is_main = False
-        # Handle the "General" option here and display relevant commands
+        
+        general_commands = [
+        cmd for cmd in self.bot.commands
+        if cmd.cog_name is not None  # Exclude commands with no cog
+        and cmd.cog_name != 'Owner_Utilities'  # Exclude commands from the Owner Utilities cog
+        ]
+        general_commands = sorted(general_commands, key=lambda cmd: cmd.name.lower())
+        
+        per_page = 5  # Number of commands to display per page
+        pages = [general_commands[i:i + per_page] for i in range(0, len(general_commands), per_page)]
+
+        if not pages:
+            await self.message.edit(embed=self.get_no_commands_embed())
+        else:
+            self.pages = pages
+            self.page_index = 0
+            await self.show_page(self.page_index)
+        
+
+    async def show_page(self, page_index, is_admin = False):
+        commands = self.pages[page_index]
+        
+        embed = None
+        if not is_admin:
+            embed = self.get_general_commands_embed(commands, page_index)
+        else:
+            embed = self.get_admin_commands_embed(commands, page_index)
+            
+        await self.message.edit(embed=embed)
+        await self.add_page_reactions()
+
+    def get_no_commands_embed(self):
+        embed = discord.Embed(title="No commands available", color=0x7e016f)
+        return embed
+    def get_no_perms_embed(self):
+        embed = discord.Embed(title="No commands available", color=0x7e016f)
+        embed.set_image(url="https://media.tenor.com/GYeAvy0kg0MAAAAC/gandalf-laughing.gif")
+        return embed
+
+    def get_general_commands_embed(self, commands, page_index):
+        embed = discord.Embed(
+            title="General Commands",
+            color=0x7e016f
+        )
+
+        for command in commands:
+            # Display the command's name in title mode with its help or description
+            embed.add_field(
+                name=command.name.title(),
+                value=command.help or "No description",
+                inline=False
+            )
+
+        # Set the page number in the footer
+        embed.set_footer(text=f"Page {page_index + 1}/{len(self.pages)}", icon_url=self.message.guild.icon.url)
+        return embed
+
+    def get_admin_commands_embed(self, commands, page_index):
+            embed = discord.Embed(
+                title="Admin Commands",
+                color=0x7e016f
+            )
+
+            for command in commands:
+                # Display the command's name in title mode with its help or description
+
+                embed.add_field(
+                    name=command.qualified_name.title(),
+                    value=command.description or "No description",
+                    inline=False
+                )
+
+            # Set the page number in the footer
+            embed.set_footer(text=f"Page {page_index + 1}/{len(self.pages)}", icon_url=self.message.guild.icon.url)
+            return embed
+
+    async def add_page_reactions(self):
+        if self.page_index > 0:
+            await self.message.add_reaction("⬅️")  # Left arrow (only if there's a previous page)
+        if len(self.pages) > 1 and self.page_index < len(self.pages) - 1:
+            await self.message.add_reaction("➡️")  # Right arrow (only if there's a next page)
 
     @menus.button("2️⃣")
     async def on_admin(self, payload):
         await self.message.clear_reactions()
         await self.message.add_reaction("↩️")
         self.is_main = False
-        # Handle the "Admin" option here and display relevant commands
+        
+        if not payload.member.guild_permissions.administrator:
+            await self.message.edit(embed=self.get_no_perms_embed())
+            return
+            
+
+        admin_commands = [command for command in self.bot.tree.walk_commands() if isinstance(command, app_commands.Command)]
+        admin_commands = sorted(admin_commands, key=lambda cmd: cmd.qualified_name.lower())
+        
+        per_page = 5  # Number of commands to display per page
+        pages = [admin_commands[i:i + per_page] for i in range(0, len(admin_commands), per_page)]
+
+        if not pages:
+            await self.message.edit(embed=self.get_no_commands_embed())
+        else:
+            self.pages = pages
+            self.page_index = 0
+            await self.show_page(self.page_index, True)
 
 
 class HelpCog(commands.Cog):
