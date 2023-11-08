5e153916a007ad7586a4d05eef2d8f5b2401ce40
Daniel Dublin <bots4fun1@gmail.com>
2023-11-09 01:26:29 +0200

    changed tmp path again

--- modules/soundcloud_promotion_checker.py
+++ modules/soundcloud_promotion_checker.py
@@ -5,7 +5,7 @@ import urlextract
 from bs4 import BeautifulSoup
 
 # Define the custom cache directory path
-custom_cache_dir = '/tmp/urlextract_cache/'
+custom_cache_dir = '/tmp/'
 
 # Initialize the URL extractor with the custom cache directory
 url_extractor = urlextract.URLExtract(cache_dir=custom_cache_dir)
