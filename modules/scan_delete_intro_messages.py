b3ca0a4e2907ba105744ab9a72f46afeca8d0c0e
Daniel Dublin <bots4fun1@gmail.com>
2023-11-25 21:58:27 +0200

    fixed 24 hour message deleter not working properly, changed logic to scan the messages every 24 hours instead of creating multiple tasks. also added a global call to bot in bot.py

--- modules/scan_delete_intro_messages.py
+++ modules/scan_delete_intro_messages.py
@@ -1,4 +1,3 @@
-from re import A
 import discord
 import asyncio
 from discord.ext import commands
@@ -6,32 +5,30 @@ from data.constants import INTRO_MUSIC
 import datetime
 
 
-async def clean_old_messages(channel):
+async def clean_old_messages(bot):
    
-    print('starting to go over intro-music')
+    print('starting up intro-music deleter')
+
+    channel = bot.get_channel(INTRO_MUSIC)
     if channel is None:
         return
     
-    now = discord.utils.utcnow()
-    async for message in channel.history(limit=None, oldest_first=True):
-        if message.author.guild_permissions.administrator:
-            continue         
+
+    while True:    
+        now = discord.utils.utcnow()
+        async for message in channel.history(limit=None, oldest_first=True):
+            if message.author.guild_permissions.administrator:
+                continue         
         
-        time_passed = now - message.created_at
-        if time_passed > datetime.timedelta(days=1):
-            try:
-                await message.delete()
-            except Exception as e:
-                print(str(e))
-        else:
-            remaining_time = datetime.timedelta(days=1) - time_passed
-            asyncio.create_task(delayed_delete(message, remaining_time.total_seconds()))
+            time_passed = now - message.created_at
+            if time_passed >= datetime.timedelta(days=1):
+                try:
+                    await message.delete()
+                except Exception as e:
+                    print(str(e))
+            else:
+                break
+        
+        await asyncio.sleep(60*60) # 1 hour
              
 
-
-async def delayed_delete(message, remaining_time):
-    try:
-        await asyncio.sleep(remaining_time)
-        await message.delete()
-    except Exception as e:
-        print(str(e))
