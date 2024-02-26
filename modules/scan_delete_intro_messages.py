8c69b885fbd546f311cfc7c037c7f089ce09f589
Daniel Dublin <bots4fun1@gmail.com>
2024-02-27 01:53:52 +0200

    converted the function into a cog

--- modules/scan_delete_intro_messages.py
+++ modules/scan_delete_intro_messages.py
@@ -1,29 +1,40 @@
 import discord
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 from data.constants import INTRO_MUSIC
 import datetime
 
-def is_a_normie(message):
+class MessageCleaner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.clean_old_messages.start()
+
+    def cog_unload(self):
+        self.clean_old_messages.cancel()
+
+    @staticmethod
+    def is_a_normie(message):
+        if not message.author.guild_permissions.administrator:
+            now = discord.utils.utcnow()
+            time_passed = now - message.created_at
+            if time_passed >= datetime.timedelta(days=1):
+                return True
+        return False
     
-    if  not message.author.guild_permissions.administrator:
-        now = discord.utils.utcnow()
-        time_passed = now - message.created_at
-        if time_passed >= datetime.timedelta(days=1):
-            return True
-       
-    return False
 
-async def clean_old_messages(bot):
-   
-    print('starting up intro-music deleter')
+    @tasks.loop(hours=1)  # Runs every hour
+    async def clean_old_messages(self):
+        if self.channel is not None:
+            await self.channel.purge(bulk=True, check=self.is_a_normie)
 
-    channel = bot.get_channel(INTRO_MUSIC)
-    if channel is None:
-        return
-    
-    while(True):
-        await channel.purge(bulk = True, check=is_a_normie)
-        await asyncio.sleep(60*60) # 1 hour
-             
+    @clean_old_messages.before_loop
+    async def before_printer(self):
+        print('starting up intro-music deleter')
+        print('waiting for the bot to be ready')
+        await self.bot.wait_until_ready()
+        print('ready')
+        self.channel = self.bot.get_channel(INTRO_MUSIC)
+        
 
+async def setup(bot):
+    await bot.add_cog(MessageCleaner(bot))
