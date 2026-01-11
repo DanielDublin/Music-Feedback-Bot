364cf235dde65d38387a6afbaa1e68e4c23676b8
Jdollx <jenniferdolldev@gmail.com>
2026-01-11 15:36:37 -0500

    cog loading issue

--- cogs/finished_music_message.py
+++ cogs/finished_music_message.py
@@ -1,18 +1,29 @@
 import discord
 import asyncio
+from discord.ext import commands, tasks
 from data.constants import FINISHED_MUSIC
 
-class FinishedMusicMessage(discord.ext.commands.Cog):
+class FinishedMusicMessage(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.stored_message_id = None
         self.stored_channel_id = FINISHED_MUSIC
+        self.message_sent = False
 
     async def cog_load(self):
-        """Called when the cog is loaded - sends the initial message."""
-        # Wait for bot to be ready before sending
-        await self.client.wait_until_ready()
-        await self.send_finished_message()
+        """Called when the cog is loaded - starts the background task."""
+        self.delete_and_repost_cycle.start()
+
+    async def cog_unload(self):
+        """Called when the cog is unloaded - stops the background task."""
+        self.delete_and_repost_cycle.cancel()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Send initial message when bot is ready."""
+        if not self.message_sent:
+            await self.send_finished_message()
+            self.message_sent = True
 
     async def send_finished_message(self):
         """Send a message to the specified channel when music finishes."""
@@ -25,41 +36,39 @@ class FinishedMusicMessage(discord.ext.commands.Cog):
         if channel:
             sent_message = await channel.send(message_text)
             self.stored_message_id = sent_message.id
-            
-            # Start the deletion/repost cycle
-            asyncio.create_task(self.delete_and_repost_cycle())
         else:
             print(f"Channel with ID {FINISHED_MUSIC} not found")
 
+    @tasks.loop(hours=24)
     async def delete_and_repost_cycle(self):
-        """Wait 24 hours, then delete and repost the message."""
-        while True:
-            # Wait 24 hours (86400 seconds)
-            await asyncio.sleep(86400)
+        """Delete and repost the message every 24 hours."""
+        channel = self.client.get_channel(self.stored_channel_id)
+        
+        if channel and self.stored_message_id:
+            try:
+                # Fetch and delete the old message
+                old_message = await channel.fetch_message(self.stored_message_id)
+                await old_message.delete()
+            except discord.NotFound:
+                print("Message not found, may have been manually deleted")
+            except discord.HTTPException as e:
+                print(f"Error deleting message: {e}")
             
-            channel = self.client.get_channel(self.stored_channel_id)
+            # Send new message
+            message_text = "**Deleted song?** If your name is green, you have the Groupies role and have 5-minute access to this channel! Stay active in the server for a few days and quickly rank up for full access."
             
-            if channel and self.stored_message_id:
-                try:
-                    # Fetch and delete the old message
-                    old_message = await channel.fetch_message(self.stored_message_id)
-                    await old_message.delete()
-                except discord.NotFound:
-                    print("Message not found, may have been manually deleted")
-                except discord.HTTPException as e:
-                    print(f"Error deleting message: {e}")
-                
-                # Send new message
-                message_text = "**Deleted song?** If your name is green, you have the Groupies role and have 5-minute access to this channel! Stay active in the server for a few days and quickly rank up for full access."
-                
-                try:
-                    new_message = await channel.send(message_text)
-                    self.stored_message_id = new_message.id
-                except discord.HTTPException as e:
-                    print(f"Error sending new message: {e}")
-            else:
-                print(f"Channel with ID {self.stored_channel_id} not found")
-                break  # Exit loop if channel is not found
+            try:
+                new_message = await channel.send(message_text)
+                self.stored_message_id = new_message.id
+            except discord.HTTPException as e:
+                print(f"Error sending new message: {e}")
+        else:
+            print(f"Channel with ID {self.stored_channel_id} not found")
+
+    @delete_and_repost_cycle.before_loop
+    async def before_cycle(self):
+        """Wait for bot to be ready before starting the loop."""
+        await self.client.wait_until_ready()
 
 
 async def setup(bot):
